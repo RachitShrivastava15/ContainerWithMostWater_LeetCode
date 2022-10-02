74db9b07379f0f078d7558f65b7b6abdaf085a81
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-01 23:43:08 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ContainerWithMostWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = {1,8,6,2,5,4,8,3,7};
+            int left = 0;
+            int right = height.Length - 1;
+
+            int res = 0;
+
+            while (left < right)
+            {
+                int minHeight = Math.Min(height[left], height[right]);
+                res = Math.Max(res, minHeight * (right - left));
+
+                if (height[left] <= height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
